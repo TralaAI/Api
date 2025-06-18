f329da36bc9b8a1bb2a25a4604aa493c967cf37e
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-18 20:43:05 +0200

    Refactor Litter model and migration files: change Type and Weather properties to string, update database schema accordingly

---
+++ Migrations/20250618180337_EnumSupport.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnumSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Weather",
+                table: "Litters",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Litters",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weather",
+                table: "Litters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Litters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Litter.cs
+++ Models/Litter.cs
@@ -2,13 +2,21 @@ using Api.Models.Enums;
 
 namespace Api.Models;
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Litter
 {
     public Guid Id { get; set; }
+
+    [Column(TypeName = "varchar(50)")]
     public LitterCategory? Type { get; set; }
+
     public DateTime TimeStamp { get; set; }
     public double Confidence { get; set; }
+
+    [Column(TypeName = "varchar(50)")]
     public WeatherCategory? Weather { get; set; } // TODO: Possibly map to WeatherCondition enum
+
     public int Temperature { get; set; }
     public string? Location { get; set; } // TODO: Possibly map to Location enum
     public bool IsHoliday { get; set; } //! Komt van externe API
