f48024330662014cab765c0fcf2d10feb5825e69
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-19 00:32:01 +0200

    Change ApiKey properties from internal to public for improved accessibility

--- ApiKeysOptions.cs
+++ ApiKeysOptions.cs
@@ -7,11 +7,11 @@ public class ApiKeysOptions
   public const string SectionName = "ApiKeys";
 
   [Required(AllowEmptyStrings = false)]
-  public string HolidayApiKey { get; internal set; } = string.Empty;
+  public string HolidayApiKey { get; set; } = string.Empty;
 
   [Required(AllowEmptyStrings = false)]
-  public string SensoringApiKey { get; internal set; } = string.Empty;
+  public string SensoringApiKey { get; set; } = string.Empty;
 
   [Required(AllowEmptyStrings = false)]
-  public string WeatherApiKey { get; internal set; } = string.Empty;
+  public string WeatherApiKey { get; set; } = string.Empty;
 }
