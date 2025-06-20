fb1a0cfe584a948a7bb0886f89d101a45a195d35
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-21 00:20:59 +0200

    Refactor PredictionRequest model: enhance Input properties with JsonPropertyName attributes for improved serialization

--- Models/PredictionRequest.cs
+++ Models/PredictionRequest.cs
@@ -1,18 +1,36 @@
+using System.Text.Json.Serialization;
+
 namespace Api.Models;
 
 public class PredictionRequest
 {
+    [JsonPropertyName("modelIndex")]
     public required string ModelIndex { get; set; }
+
+    [JsonPropertyName("inputs")]
     public required List<Input> Inputs { get; set; }
 }
 
 public class Input
 {
+    [JsonPropertyName("day_of_week")]
     public required int DayOfWeek { get; set; }
+
+    [JsonPropertyName("month")]
     public required int Month { get; set; }
+
+    [JsonPropertyName("holiday")]
     public required bool Holiday { get; set; }
+
+    [JsonPropertyName("weather")]
     public required int Weather { get; set; }
+
+    [JsonPropertyName("temperature_celcius")]
     public required int TemperatureCelcius { get; set; }
+
+    [JsonPropertyName("is_weekend")]
     public required bool IsWeekend { get; set; }
+
+    [JsonPropertyName("label")]
     public required string Label { get; set; }
 }
