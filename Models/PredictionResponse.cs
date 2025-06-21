e5f7e0a010c1bd6de1aaaa33373fabb3086de4c0
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-21 17:35:58 +0200

    Refactor PredictionResponse model: simplify structure by removing unnecessary classes and properties

--- Models/PredictionResponse.cs
+++ Models/PredictionResponse.cs
@@ -1,37 +1,13 @@
-using System.Text.Json.Serialization;
+namespace Api.Models;
 
-namespace Api.Models
+public class PredictionResponse
 {
-    public class PredictionResponse
-    {
-        [JsonPropertyName("predictions")]
-        public List<PredictionDay> Predictions { get; set; } = [];
-    }
+    public required DateTime Date { get; set; }
+    public required List<Prediction> Predictions { get; set; }
+}
 
-    public class PredictionDay
-    {
-        [JsonPropertyName("date")]
-        public DateTime Date { get; set; }
-
-        [JsonPropertyName("predictions")]
-        public WastePrediction Waste { get; set; } = new();
-    }
-
-    public class WastePrediction
-    {
-        [JsonPropertyName("plastic")]
-        public double Plastic { get; set; }
-
-        [JsonPropertyName("paper")]
-        public double Paper { get; set; }
-
-        [JsonPropertyName("metal")]
-        public double Metal { get; set; }
-
-        [JsonPropertyName("glass")]
-        public double Glass { get; set; }
-
-        [JsonPropertyName("organic")]
-        public double Organic { get; set; }
-    }
+public class Prediction
+{
+    public required string Type { get; set; }
+    public required double Value { get; set; }
 }
