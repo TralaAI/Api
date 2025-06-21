28beb13c17d38bad4abd1b97867abe0f6f580262
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-21 17:27:02 +0200

    Refactor PredictionResponse model: restructure classes for improved clarity and organization

--- Models/PredictionResponse.cs
+++ Models/PredictionResponse.cs
@@ -1,30 +1,37 @@
 using System.Text.Json.Serialization;
 
-namespace Api.Models;
-
-public class PredictionResponse
+namespace Api.Models
 {
-    [JsonPropertyName("predictions")]
-    public List<WastePrediction> Predictions { get; set; } = new();
-}
+    public class PredictionResponse
+    {
+        [JsonPropertyName("predictions")]
+        public List<PredictionDay> Predictions { get; set; } = [];
+    }
 
-public class WastePrediction
-{
-    [JsonPropertyName("date")]
-    public DateOnly Date { get; set; }
+    public class PredictionDay
+    {
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
 
-    [JsonPropertyName("plastic")]
-    public double Plastic { get; set; }
+        [JsonPropertyName("predictions")]
+        public WastePrediction Waste { get; set; } = new();
+    }
 
-    [JsonPropertyName("paper")]
-    public double Paper { get; set; }
+    public class WastePrediction
+    {
+        [JsonPropertyName("plastic")]
+        public double Plastic { get; set; }
 
-    [JsonPropertyName("metal")]
-    public double Metal { get; set; }
+        [JsonPropertyName("paper")]
+        public double Paper { get; set; }
 
-    [JsonPropertyName("glass")]
-    public double Glass { get; set; }
+        [JsonPropertyName("metal")]
+        public double Metal { get; set; }
 
-    [JsonPropertyName("organic")]
-    public double Organic { get; set; }
+        [JsonPropertyName("glass")]
+        public double Glass { get; set; }
+
+        [JsonPropertyName("organic")]
+        public double Organic { get; set; }
+    }
 }
