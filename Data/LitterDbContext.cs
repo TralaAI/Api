24995176b4189a96c15b11e6b679fe7e7aa48cd9
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-21 21:38:27 +0200

    Add LitterDbContext configuration for Camera entity: define relationship and cascade delete behavior

--- Data/LitterDbContext.cs
+++ Data/LitterDbContext.cs
@@ -7,5 +7,17 @@ namespace Api.Data;
 public class LitterDbContext(DbContextOptions<LitterDbContext> options) : DbContext(options)
 {
   public DbSet<Litter> Litters { get; set; }
+  public DbSet<Camera> Cameras { get; set; }
   public DbSet<ApiKey> ApiKeys { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<Litter>()
+      .HasOne<Camera>()
+      .WithMany()
+      .HasForeignKey(l => l.CameraId)
+      .OnDelete(DeleteBehavior.Cascade);
+  }
 }
