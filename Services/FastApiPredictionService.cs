ea2e796e241cebbb79775dd3ce0593ce694e1361
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-21 20:44:51 +0200

    Made a POST to a GET for Wouter!

--- Services/FastApiPredictionService.cs
+++ Services/FastApiPredictionService.cs
@@ -66,7 +66,7 @@ namespace Api.Services
         {
             try
             {
-                var response = await _httpClient.PostAsync("/status", null);
+                var response = await _httpClient.GetAsync("/status");
                 response.EnsureSuccessStatusCode();
                 return true;
             }
